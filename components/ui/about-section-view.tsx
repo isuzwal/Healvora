53dd8e91dc5546f624a55de3dbf21148efcb6071
isuzwal <ujjwalgaihre0@gmail.com>
2026-02-01 19:12:56 +0530

    About Section done with soome-animation

--- components/ui/about-section-view.tsx
+++ components/ui/about-section-view.tsx
@@ -1,47 +1,91 @@
-import { ArrowRight } from "lucide-react";
+import { ArrowRight, Check, Flower } from "lucide-react";
 import Image from "next/image";
 
 export const AboutSection = () => {
   return (
-    <div className="py-12 w-full rounded-t-4xl bg-primary/70">
-      <div className="flex flex-col justify-start items-start px-4">
-        <span
-          className="rounded-4xl border text-white  border-foreground bg-foreground  
-            font-medium  px-6 py-1.5 text-sm font-sans"
-        >
-          About us
-        </span>
-        <div className="  sm:flex-row flex flex-col-reverse   justify-between gap-4 w-full   items-center">
-          <div className=" w-full flex flex-col  justify-start items-start">
-            <div className="flex  w-full  text-white flex-col gap-4">
-              <p className="text-4xl md:text-6xl  w-full  tracking-tight font-medium font-sans">
-                Dedicated to Your Health, Every Step of the Way
-              </p>
-
-              <p className="font-medium  text-[12px] md:text-[14px] text-black/70 max-w-md">
-                Healvora is an AI-powered hospital platform delivering real-time
-                medical support, combining intelligent technology with
-                compassionate human interaction.
-              </p>
-            </div>
-            <span
-              className="rounded-4xl group  flex  items-center gap-2 mt-6 text-white bg-primary/70 border-primary/70
-            font-medium border px-5 py-1.5 text-sm font-sans"
-            >
-              Learn more{" "}
-              <ArrowRight className="size-3.5 group-hover:translate-x-1 duration-300 ease-in-out  transition-all" />
-            </span>
-          </div>
-
-          <div className="mt-2 relative  border  max-w-md w-full  flex justify-center items-center  rounded-lg overflow-hidden">
+    <div className="py-6 w-full px-2 ">
+      <div className=" flex flex-col  sm:flex-row max-w-6xl  w-full mx-auto  justify-between items-center gap-6">
+        <div className="  flex flex-col  items-start  w-full  sm:w-1/2 justify-start  gap-2 p-1 ">
+          <span className="text-[14px] font-sans text-white rounded-md px-4 py-1 border bg-emerald-600  border-primary flex justify-center items-center  font-medium">
+            About us
+          </span>
+          <p className="text-neutral-800/95 font-sans   font-medium  tracking-tighter text-2xl md:text-4xl text-start">
+            Advanced medical <br /> care with compassion
+          </p>
+          <div className=" group relative overflow-hidden  rounded-md  border  border-neutral-100 flex justify-center items-center ">
             <Image
-              src="/images/second.png"
-              alt="Doctor"
-              width={200}
-              height={200}
-              className="object-cover rounded-lg  w-full  max-h-200 "
+              src="/images/doctor-1.png"
+              alt="doctor-image"
+              height={420}
+              width={420}
               loading="lazy"
+              className="object-cover rounded-md h-full w-full"
             />
+            <div
+              className="absolute    inset-x-0 bottom-0 h-30  duration-300 ease-in-out transition-all
+            bg-gradient-to-t from-primary/60 via-primary/40 to-transparent"
+            />
+          </div>
+        </div>
+        <div className="flex flex-col w-full  sm:w-1/2  justify-center items-center   gap-2 ">
+          <div className=" group relative w-full overflow-hidden h-72 rounded-md  border  border-neutral-100 flex justify-center items-center ">
+            <Image
+              src="/images/doctor-2.png"
+              alt="doctor-image"
+              fill
+              loading="lazy"
+              className="object-cover rounded-md h-auto max-h-74 w-full"
+            />
+          </div>
+          <div className=" w-full flex-col  sm:flex-row flex gap-2  justify-between items-center">
+            <div className="w-full flex flex-col  gap-3 p-1">
+              <div className="flex flex-col gap-2    justify-start items-start">
+                <p className="font-sans text-[16px] flex gap-2 justify-center items-center font-medium text-neutral-800  ">
+                  <span className="w-4 h-4 rounded-full flex justify-center items-center bg-green-400 border border-green-300">
+                    <Check className=" text-white size-3 " />
+                  </span>
+                  Expert Medical Team
+                </p>
+                <p className="font-sans flex gap-2 justify-center items-center font-medium text-neutral-800 text-[16px] ">
+                  <span className="w-4 h-4 rounded-full flex justify-center items-center bg-green-400 border border-green-300">
+                    <Check className=" text-white size-3 " />
+                  </span>
+                  AI-Powered Health Care
+                </p>
+                <p className="font-sans flex gap-2 justify-center items-center font-medium text-neutral-800 text-[16px] ">
+                  <span className="w-4 h-4 rounded-full flex justify-center items-center bg-green-400 border border-green-300">
+                    <Check className=" text-white size-3 " />
+                  </span>
+                  Smart Doctor Matching
+                </p>
+                <p className="font-sans flex gap-2 justify-center items-center font-medium text-neutral-800 text-[16px] ">
+                  <span className="w-4 h-4  rounded-full flex justify-center items-center bg-green-400 border border-green-300">
+                    <Check className=" text-white size-3 " />
+                  </span>
+                  24/7 Emergency Support
+                </p>
+              </div>
+              <div>
+                <button className=" mt-2 text-[16px] font-sans font-medium text-white rounded-md border border-primary bg-emerald-600 tracking-tighter px-4 py-1.5">
+                  More About us
+                </button>
+              </div>
+            </div>
+            <div
+              className="h-full py-8 w-full rounded-xl 
+              bg-gradient-to-br from-emerald-400 via-emerald-500 to-emerald-600
+             shadow-emerald-500/30 flex justify-center items-center transition-all duration-300 ease-out"
+            >
+              <div className=" flex w-full flex-col  gap-2 justify-center items-center">
+                <span className="w-12 h-12 bg-white  flex  justify-center items-center rounded-full border border-neutral-50 ">
+                  <Flower className="size-8 text-green-400" />
+                </span>
+                <p className="text-center  flex-col font-sans font-medium text-white text-[16px] ">
+                  <strong className=" font-medium text-2xl ">2+</strong> <br />
+                  Years of Medical <br /> Experience
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
--- components/ui/hero-section-view.tsx
+++ components/ui/hero-section-view.tsx
@@ -1,14 +1,64 @@
+"use client";
 import { ArrowRight, Brain } from "lucide-react";
 import Image from "next/image";
 import { TextAnimate } from "./text-animate";
+import { motion, Variants } from "motion/react";
+
 export const HeroSection = () => {
+  const word = `Comprehensive, compassionate healthcare services designed to support
+    your fmaily's well-begin at every stage of life`;
+
+  const containerVariants: Variants = {
+    hidden: {},
+    show: {
+      transition: {
+        staggerChildren: 0.15,
+        delayChildren: 0.3,
+      },
+    },
+  };
+
+  const itemVariants: Variants = {
+    hidden: {
+      opacity: 0,
+      y: 10,
+      filter: "blur(10px)",
+    },
+    show: {
+      opacity: 1,
+      y: 0,
+      filter: "blur(0px)",
+      transition: {
+        duration: 0.4,
+        ease: "easeOut",
+      },
+    },
+  };
+
   return (
-    <div className="w-full py-20 ">
-      <div className="flex flex-col gap-2 max-w-5xl mx-auto  items-center justify-center  w-full">
-        <span className="px-4.5 py-0.5 gap-1 bg-emerald-50 border-emerald-100 shadow-[inset_1px_2px_1px_rgba(16,_170,_120,_0.2)] inline-flex border rounded-xl font-sans font-medium text-xs items-center text-emerald-600">
+    <div className="w-full   sm:py-16 ">
+      <div className="flex py-8 sm:py-0  flex-col gap-2 max-w-5xl mx-auto  items-center justify-center  w-full">
+        <motion.span
+          initial={{
+            opacity: 0,
+            y: 5,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            opacity: 1,
+            filter: "blur(0px)",
+            y: 0,
+          }}
+          transition={{
+            duration: 0.3,
+            ease: "easeIn",
+            delay: 0.1,
+          }}
+          className="px-4.5 py-0.5 gap-1 bg-emerald-50 border-emerald-100 shadow-[inset_1px_2px_1px_rgba(16,_170,_120,_0.2)] inline-flex border rounded-xl font-sans font-medium text-xs items-center text-emerald-600"
+        >
           <Brain className="size-3.5 text-emerald-600" />
           Healvora AI Driven
-        </span>
+        </motion.span>
         <TextAnimate
           animate="fadeIn"
           as="h1"
@@ -16,56 +66,85 @@ export const HeroSection = () => {
         >
           Healvora Trutworthy Care for you and Your Famaily
         </TextAnimate>
-        <div className=" max-w-lg dark:text-neutral-600 text-neutral-600 font-medium  font-sans flex w-full  justify-center  item-center ">
-          <p className="text-center text-[14px]">
-            Comprehensive, compassionate healthcare services designed to support
-            your fmaily&apos;s well-begin at every stage of life
-          </p>
-        </div>
-        <div className="flex   flex-col sm:flex-row mt-4 gap-4  max-w-2xl w-full justify-center items-center">
-          <button className="flex  justify-center group text-sm items-center  cursor-pointer gap-1.5 rounded-lg px-6 py-1.5  font-medium dark:hover:text-neutral-300 duration-300 ease-in-out transition-all  text-neutral-100 bg-primary border border-primary/70 hover:bg-primary/80">
-            Book Appointment{" "}
-            <ArrowRight className=" group-hover:translate-x-2 duration-300  ease-in-out transition-all  size-4.5" />
-          </button>
-          <div className="flex  justify-center    max-w-[20rem] w-full">
-            <div className="relative flex max-w-22  w-full  items-center ">
-              <div className=" absolute z-30 left-0 w-7 h-7    overflow-hidden flex justify-center items-center rounded-full ">
-                <Image
-                  src={"/images/first.png"}
-                  fill
-                  loading="lazy"
-                  alt="first-pic"
-                  className="object-cover border border-neutral-200  shadow w-full h-full rounded-full"
-                />
-              </div>
-              <div className="w-7 h-7  overflow-hidden absolute z-20 left-6 flex justify-center items-center rounded-full ">
-                <Image
-                  src={"/images/lady.png"}
-                  fill
-                  loading="lazy"
-                  alt="first-pic"
-                  className="object-cover border border-neutral-200  shadow w-full h-full rounded-full"
-                />
-              </div>
+        <motion.div
+          variants={containerVariants}
+          initial="hidden"
+          animate="show"
+          className=" flex flex-col gap-1  max-w-xl w-full justify-center items-center "
+        >
+          <motion.div className=" max-w-lg dark:text-neutral-600 text-neutral-600 font-medium  font-sans flex w-full  justify-center  item-center ">
+            <motion.p
+              variants={itemVariants}
+              className="text-center inline-flex text-[14px]"
+            >
+              {word}
+            </motion.p>
+          </motion.div>
+          <motion.div
+            variants={itemVariants}
+            className="flex   flex-col sm:flex-row mt-4 gap-4  max-w-2xl w-full justify-center items-center"
+          >
+            <button className="flex  justify-center group text-sm items-center  cursor-pointer gap-1.5 rounded-lg px-6 py-1.5  font-medium dark:hover:text-neutral-300 duration-300 ease-in-out transition-all  text-neutral-100 bg-primary border border-primary/70 hover:bg-primary/80">
+              Book Appointment{" "}
+              <ArrowRight className=" group-hover:translate-x-2 duration-300  ease-in-out transition-all  size-4.5" />
+            </button>
+            <div className="flex sm:mt-0 mt-2  justify-center   items-center px-2   max-w-[20rem] w-full">
+              <div className="relative flex max-w-22  w-full  items-center ">
+                <div className=" absolute z-30 left-0 w-7 h-7    overflow-hidden flex justify-center items-center rounded-full ">
+                  <Image
+                    src={"/images/first.png"}
+                    fill
+                    loading="lazy"
+                    alt="first-pic"
+                    className="object-cover border border-neutral-200  shadow w-full h-full rounded-full"
+                  />
+                </div>
+                <div className="w-7 h-7  overflow-hidden absolute z-20 left-6 flex justify-center items-center rounded-full ">
+                  <Image
+                    src={"/images/lady.png"}
+                    fill
+                    loading="lazy"
+                    alt="first-pic"
+                    className="object-cover border border-neutral-200  shadow w-full h-full rounded-full"
+                  />
+                </div>
 
-              <div className="w-7 h-7 absolute z-10 right-3  overflow-hidden flex justify-center items-center rounded-full ">
-                <Image
-                  src={"/images/third.png"}
-                  fill
-                  loading="lazy"
-                  alt="first-pic"
-                  className="object-cover   border border-neutral-200   shadow w-full h-full rounded-full"
-                />
+                <div className="w-7 h-7 absolute z-10 right-3  overflow-hidden flex justify-center items-center rounded-full ">
+                  <Image
+                    src={"/images/third.png"}
+                    fill
+                    loading="lazy"
+                    alt="first-pic"
+                    className="object-cover   border border-neutral-200   shadow w-full h-full rounded-full"
+                  />
+                </div>
               </div>
+              <p className="flex w-full gap-0.5   items-center text-[14px] font-sans font-medium text-neutral-950">
+                <strong>200+ </strong>Appoinments Sucessfully
+              </p>
             </div>
-            <p className="flex w-full gap-0.5   items-center text-[14px] font-sans font-medium text-neutral-950">
-              <strong>200+ </strong>Appoinments Sucessfully
-            </p>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </div>
       <div className="w-full p-1.5  mt-4  sm:mt-6 flex justify-center items-center  py-16 ">
-        <div className=" relative overflow-hidden  flex justify-center items-center  max-w-5xl  mx-auto w-full rounded-lg  bg-neutral-200 border border-neutral-200">
+        <motion.div
+          initial={{
+            y: 0,
+            opacity: 0,
+            filter: "blur(10px)",
+          }}
+          animate={{
+            y: -8,
+            filter: "blur(0px)",
+            opacity: 1,
+          }}
+          transition={{
+            duration: 0.5,
+            delay: 0.6,
+            ease: "easeIn",
+          }}
+          className=" relative overflow-hidden  flex justify-center items-center  max-w-5xl  mx-auto w-full rounded-lg  bg-neutral-200 border border-neutral-200"
+        >
           <Image
             src={"/images/Hero.png"}
             alt="hero-section"
@@ -74,7 +153,7 @@ export const HeroSection = () => {
             loading="lazy"
             className="rounded-lg object-contain h-full w-full max-h[40rem]"
           />
-        </div>
+        </motion.div>
       </div>
     </div>
   );
